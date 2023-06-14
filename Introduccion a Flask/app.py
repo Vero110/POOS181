c7e4b4f1ed6505ad4ddab00393c1bc6820063c01
Vero110 <veronicaalmaraz59@gmail.com>
2023-06-14 17:09:54 -0600

    Rutas y Coneccion a la BD
    Finalizamos la practica No.5

--- Introduccion a Flask/app.py
+++ Introduccion a Flask/app.py
@@ -1,11 +1,30 @@
+#importacion de la paqueteria del framework
 from flask import Flask 
+from flask_mysqldb import MySQL
 
+#inicializacion de la aplicacion: Señalar que va a trabajar con flask 
 app = Flask(__name__)
+app.config['MYSQL_HOST']= 'localhost'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']= '11082003'
+app.config['MYSQL_DB']= 'dbFlask'
+mysql = MySQL(app)
 
-@app.route('/')
-def index():
+
+#declaracion de la inicializacion de la ruta http://localhost:5000
+@app.route('/') #ruta principal
+def index(): 
     return "Hola Mundo FLASK"
 
+@app.route('/guardar') 
+def guardar(): 
+    return "Se guardo en la BD"
+
+@app.route('/eliminar') 
+def eliminar(): 
+    return "Se elimino en la BD"
+
+#ejecucion del servidor en el puerto 5000
 if __name__ == '__main__': 
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
     
